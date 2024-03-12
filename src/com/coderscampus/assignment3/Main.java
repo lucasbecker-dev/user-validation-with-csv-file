daf3dea56de74ecbc9a97092c84e5b6a427f5155
Lucas Becker <lucasbecker.dev@gmail.com>
2024-03-12 16:20:32 -0400

    corrected file path

--- src/com/coderscampus/assignment3/Main.java
+++ src/com/coderscampus/assignment3/Main.java
@@ -3,7 +3,7 @@ package com.coderscampus.assignment3;
 public class Main {
     public static void main(String[] args) {
         final int LINES_TO_READ = 4;
-        final String DATA_FILEPATH = "./data.txt";
+        final String DATA_FILEPATH = "src/com/coderscampus/assignment3/data.txt";
         User[] users = UserService.parseFileIntoUsers(DATA_FILEPATH, LINES_TO_READ);
     }
 }
