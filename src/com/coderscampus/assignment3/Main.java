9fc7702b8b4e827125f593aa3acca0a9cede4ea8
Lucas Becker <lucasbecker.dev@gmail.com>
2024-03-12 16:51:19 -0400

    added global Scanner to main

--- src/com/coderscampus/assignment3/Main.java
+++ src/com/coderscampus/assignment3/Main.java
@@ -1,9 +1,14 @@
 package com.coderscampus.assignment3;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         final int LINES_TO_READ = 4;
         final String DATA_FILEPATH = "src/com/coderscampus/assignment3/data.txt";
+        Scanner scanner = new Scanner(System.in);
         User[] users = UserService.parseFileIntoUsers(DATA_FILEPATH, LINES_TO_READ);
+
+        scanner.close();
     }
 }
