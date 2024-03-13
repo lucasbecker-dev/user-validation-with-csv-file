aa43e9221223909c53800c90128c537a1aa65399
Lucas Becker <lucasbecker.dev@gmail.com>
2024-03-13 00:21:21 -0400

    added exception catching for userLogin()

--- src/com/coderscampus/assignment3/Main.java
+++ src/com/coderscampus/assignment3/Main.java
@@ -6,9 +6,12 @@ public class Main {
     public static void main(String[] args) {
         final int LINES_TO_READ = 4;
         final String DATA_FILEPATH = "src/com/coderscampus/assignment3/data.txt";
-        final Scanner scanner = new Scanner(System.in);
-        User[] users = UserService.parseFileIntoUsers(DATA_FILEPATH, LINES_TO_READ);
-        UserCredentials userCredentials = UserService.getUserCredentials(scanner);
-        scanner.close();
+        try (Scanner scanner = new Scanner(System.in)) {
+            final User[] users = UserService.parseFileIntoUsers(DATA_FILEPATH, LINES_TO_READ);
+            UserService.userLogin(scanner, users);
+        } catch (NullPointerException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Exiting program due to error.");
+        }
     }
 }
