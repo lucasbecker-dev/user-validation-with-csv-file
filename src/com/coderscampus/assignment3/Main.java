9dcd361234d0275753619a262c4bb0f5acd47d06
Lucas Becker <lucasbecker.dev@gmail.com>
2024-03-12 22:39:55 -0400

    added getUserCredentials call to main()

--- src/com/coderscampus/assignment3/Main.java
+++ src/com/coderscampus/assignment3/Main.java
@@ -6,9 +6,9 @@ public class Main {
     public static void main(String[] args) {
         final int LINES_TO_READ = 4;
         final String DATA_FILEPATH = "src/com/coderscampus/assignment3/data.txt";
-        Scanner scanner = new Scanner(System.in);
+        final Scanner scanner = new Scanner(System.in);
         User[] users = UserService.parseFileIntoUsers(DATA_FILEPATH, LINES_TO_READ);
-
+        UserCredentials userCredentials = UserService.getUserCredentials(scanner);
         scanner.close();
     }
 }
