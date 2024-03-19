933c5d9f398203179bc2e0d779c980b5078b982a
Lucas Becker <lucasbecker.dev@gmail.com>
2024-03-19 14:21:03 -0400

    Main now accommodates code review feedback changes

--- src/com/coderscampus/assignment3/Main.java
+++ src/com/coderscampus/assignment3/Main.java
@@ -6,12 +6,7 @@ public class Main {
     public static void main(String[] args) {
         final int LINES_TO_READ = 4;
         final String DATA_FILEPATH = "src/com/coderscampus/assignment3/data.txt";
-        try (Scanner scanner = new Scanner(System.in)) {
-            final User[] users = UserService.parseFileIntoUsers(DATA_FILEPATH, LINES_TO_READ);
-            UserService.userLogin(scanner, users);
-        } catch (NullPointerException e) {
-            System.out.println(e.getMessage());
-            System.out.println("Exiting program due to error.");
-        }
+        UserService.loadUsers(DATA_FILEPATH, LINES_TO_READ);
+        UserLoginService.userLogin();
     }
 }
